753e80278a83cb01834a14d7153e4cf80b79bd22
Cristhian <galax13a@yahoo.es>
2021-12-01 17:28:14 -0500

    menu delete placa

--- resources/views/livewire/clientes/view.blade.php
+++ resources/views/livewire/clientes/view.blade.php
@@ -56,8 +56,8 @@
 								@foreach($this->getplaca($row->id) as $placa)
 								
 										<img src="/css/cars/bike{{$placa->icon}}.svg "  width="33" height="33" alt="">
-										<span class="badge rounded-pill ">	{{ Str::upper($placa->name) }}
-										<a class="btn btn-light " onclick="confirm('Confirm Delete placa id {{$placa->name}}? \nDeleted placa cannot be recovered!')||event.stopImmediatePropagation()" wire:click="deleteplaca({{$placa->id}})"><i class="fa fa-trash" title="Eliminar ({{$placa->name}})"></i>  </a>   
+										<span class="badge  btn btn-dark ">	{{ Str::upper($placa->name) }}
+										<a class="btn btn-danger " onclick="confirm('Confirm Delete placa id {{$placa->name}}? \nDeleted placa cannot be recovered!')||event.stopImmediatePropagation()" wire:click="deleteplaca({{$placa->id}})"><i class="fa fa-times" title="Eliminar ({{$placa->name}})"></i>  </a>   
 								</span>
 										@endforeach
 							</td>
