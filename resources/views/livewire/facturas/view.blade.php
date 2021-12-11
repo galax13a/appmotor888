4044631e551a017b3a8780a06e96264735d05c72
Cristhian <galax13a@yahoo.es>
2021-12-10 22:08:47 -0500

    factury close

--- resources/views/livewire/facturas/view.blade.php
+++ resources/views/livewire/facturas/view.blade.php
@@ -134,7 +134,7 @@
                 <div class="card-body">
                     @include('livewire.facturas.operario')
                     @include('livewire.facturas.update')
-                    @include('livewire.facturas.closefactory')
+                  
                     @include('livewire.facturas.payment')
                    
             
