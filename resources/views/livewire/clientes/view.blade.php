addfed598b5fcf411f37cb25383980d834c6b5d7
Cristhian <galax13a@yahoo.es>
2022-03-03 15:10:29 -0500

    clientes

--- resources/views/livewire/clientes/view.blade.php
+++ resources/views/livewire/clientes/view.blade.php
@@ -95,7 +95,7 @@
 								@if($row->status == 0)
 								<input  class="form-check-input btn-danger" type="checkbox" role="switch" id="status" wire:click="ckeking({{$row->id}}, 1)" >
 								@elseif ($row->status == 1)
-								<input onclick="confirm('Confirm Delete Factura id {{ $row->id }}? \nDeleted cliente cannot be recovered!')||event.stopImmediatePropagation()" class="form-check-input" type="checkbox" role="switch" id="status" checked wire:click="ckeking({{$row->id}}, 0)">
+								<input onclick="confirm('Confirm Delete Cliente id {{ $row->id }}? \nDeleted cliente cannot be recovered!')||event.stopImmediatePropagation()" class="form-check-input" type="checkbox" role="switch" id="status" checked wire:click="ckeking({{$row->id}}, 0)">
 								@endif
 							</div>
 							</div>
